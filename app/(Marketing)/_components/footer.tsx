445ecd69a348d70a7d931cac1b18bbc49e28184a
SOMU SINGH <somusingh0110@gmail.com>
2024-08-10 19:13:10 +0530

    Add marketing Page

---
+++ app/(Marketing)/_components/footer.tsx
@@ -0,0 +1,18 @@
+import { Logo } from "./logo";
+import { Button } from "@/components/ui/button";
+
+export const Footer = () => {
+  return (
+    <div className="flex items-center w-full p-6 bg-background z-50">
+      <Logo />
+      <div className="w-full flex justify-between md:justify-end md:ml-auto items-center gap-x-2 text-muted-foreground">
+        <Button variant="ghost" size="sm">
+          Privacy Policy
+        </Button>
+        <Button variant="ghost" size="sm">
+          Terms & Conditions
+        </Button>
+      </div>
+    </div>
+  );
+};
