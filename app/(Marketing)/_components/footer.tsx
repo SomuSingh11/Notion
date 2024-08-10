8f06c7d9634373398472c4d198cbe9843146dcc1
SOMU SINGH <somusingh0110@gmail.com>
2024-08-10 22:10:50 +0530

    Add Dark Mode to the App

--- app/(Marketing)/_components/footer.tsx
+++ app/(Marketing)/_components/footer.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 
 export const Footer = () => {
   return (
-    <div className="flex items-center w-full p-6 bg-background z-50">
+    <div className="flex items-center w-full p-6 bg-background z-50 dark:bg-[#1F1F1F]">
       <Logo />
       <div className="w-full flex justify-between md:justify-end md:ml-auto items-center gap-x-2 text-muted-foreground">
         <Button variant="ghost" size="sm">
