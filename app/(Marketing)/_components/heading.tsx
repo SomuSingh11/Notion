719312ea6971da7ab12d482052f04f30fb012ff2
SOMU SINGH <somusingh0110@gmail.com>
2024-08-11 12:05:53 +0530

    Implement Authentication Features in Header (Marketing Page)

--- app/(Marketing)/_components/heading.tsx
+++ app/(Marketing)/_components/heading.tsx
@@ -1,9 +1,15 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
 import { ArrowRight } from "lucide-react";
+import { useConvexAuth } from "convex/react";
+import { SignInButton } from "@clerk/clerk-react";
+import Link from "next/link";
+
+import { Spinner } from "@/components/spinner";
+import { Button } from "@/components/ui/button";
 
 export const Heading = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
   return (
     <div className="max-w-3xl space-y-4">
       <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
@@ -14,10 +20,30 @@ export const Heading = () => {
         Jotion: The workspace that brings everything <br />
         together for smoother workflows.
       </h3>
-      <Button>
-        Enter Jotion
-        <ArrowRight className="h-4 w-4 ml-2" />
-      </Button>
+
+      {isLoading && (
+        <div className="flex items-center justify-center w-full">
+          <Spinner size="lg" />
+        </div>
+      )}
+
+      {isAuthenticated && !isLoading && (
+        <Button asChild>
+          <Link href="/documents">
+            Enter Jotion
+            <ArrowRight className="h-4 w-4 ml-2" />
+          </Link>
+        </Button>
+      )}
+
+      {!isAuthenticated && !isLoading && (
+        <SignInButton mode="modal">
+          <Button>
+            {" "}
+            Get Jotion free <ArrowRight className="h-4 w-4 ml-2" />{" "}
+          </Button>
+        </SignInButton>
+      )}
     </div>
   );
 };
